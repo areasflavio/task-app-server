b73d6f5ea12147b1696362431e9a18a6a3f92a67
Flávio Arêas <vflavio9@gmail.com>
2021-05-07 20:04:24 -0300

    refactor: handle promises with async/await

--- src/index.js
+++ src/index.js
@@ -30,95 +30,90 @@ app.post('/users', (req, res) => {
     });
 });
 
-app.get('/users', (req, res) => {
-  User.find({})
-    .then((users) => {
-      return res.json(users);
-    })
-    .catch((error) => {
-      return res.status(500).json(error);
-    });
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    return res.json(users);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const { id } = req.params;
 
-  User.findById(id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
+  try {
+    const user = await User.findById(id);
 
-      return res.json(user);
-    })
-    .catch((error) => {
-      return res.status(500).json(error);
-    });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    return res.json(user);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const { description, completed } = req.body;
 
-  const task = new Task({ description, completed });
+  try {
+    const task = new Task({ description, completed });
 
-  task
-    .save()
-    .then(() => {
-      return res.status(201).json(task);
-    })
-    .catch((error) => {
-      return res.status(400).json(error);
-    });
+    await task.save();
+
+    return res.status(201).json(task);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+});
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+
+    return res.json(tasks);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+app.get('/tasks/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const task = await Task.findById(id);
+
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' });
+    }
+
+    return res.json(task);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const task = await Task.findByIdAndDelete(id);
+
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' });
+    }
+    const count = await Task.countDocuments({ completed: false });
+    console.log(count);
+
+    return res.json();
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}...`);
-});
-app.get('/tasks', (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      return res.json(tasks);
-    })
-    .catch((error) => {
-      return res.status(500).json(error);
-    });
-});
-
-app.get('/tasks/:id', (req, res) => {
-  const { id } = req.params;
-
-  Task.findById(id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).json({ error: 'Task not found' });
-      }
-
-      return res.json({ task });
-    })
-    .catch((error) => {
-      return res.status(500).json(error);
-    });
-});
-
-app.delete('/tasks/:id', (req, res) => {
-  const { id } = req.params;
-
-  Task.findByIdAndDelete(id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).json({ error: 'Task not found' });
-      }
-
-      console.log(task);
-
-      return Task.countDocuments({ completed: false });
-    })
-    .then((result) => {
-      console.log(result);
-
-      return res.json();
-    })
-    .catch((error) => {
-      return res.status(500).json(error);
-    });
 });
