b38e4b42736b31d5e5ea5eb7a50d047a30135d46
Flávio Arêas <vflavio9@gmail.com>
2021-05-08 22:15:47 -0300

    feat: hash password middleware

--- src/db/models/Task.js
+++ src/db/models/Task.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const Task = mongoose.model('Task', {
+const taskSchema = mongoose.Schema({
   description: {
     type: String,
     required: true,
@@ -11,5 +11,11 @@ const Task = mongoose.model('Task', {
     default: false,
   },
 });
+
+taskSchema.pre('save', async function (next) {
+  next();
+});
+
+const Task = mongoose.model('Task', taskSchema);
 
 module.exports = Task;
